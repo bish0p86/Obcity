c67669c4f8f55db267b3ce1c7af640ad0e301650
Ben Bishop <bish0p@hotmail.com>
2014-10-11 21:33:09 +0100

    updated

--- www/js/services.js
+++ www/js/services.js
@@ -54,7 +54,7 @@ service('pedometer', [function() {
 
 
   function getMonth(date) {
-    var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
 
     return months[date.getMonth()];
   };
